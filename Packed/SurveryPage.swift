8007c3ac6771dd412570a54880086adca20c5abd
Dhriti Burugupalli <burugupallidhriti@gmail.com>
2025-07-29 12:01:55 -0700

    Added one dropdown

--- Packed/SurveryPage.swift
+++ Packed/SurveryPage.swift
@@ -11,11 +11,32 @@ struct SurveryPage: View {
     var body: some View {
         VStack {
             Text("Create New List")
-                .font(.title)
-                .fontWeight(.bold)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
                 .foregroundColor(Color(red: 0.195, green: 0.648, blue: 0.859))
+                .padding(.vertical, 100.0)
             Text("TYPE OF VACATION")
                 .foregroundColor(Color(red: 0.561, green: 0.557, blue: 0.557))
+            Menu {
+                Button(action: { }, label: { Text("Tropical Vacation")
+                })
+                Button(action: { }, label: { Text("Mountain Vacation")
+                })
+                Button(action: { }, label: { Text("Ski Trip")
+                })
+                Button(action: { }, label: { Text("City Vacation")
+                })
+                Button(action: { }, label: { Text("Business Trip")
+                })
+            } label: {
+                Label(
+                    title: {Text("Tropical Vacation")
+                        .foregroundColor(Color(red: 0.561, green: 0.561, blue: 0.561))},
+                    icon: {Image("arrow")
+                        }
+                )
+            }
+            Spacer()
         }
     }
 }
