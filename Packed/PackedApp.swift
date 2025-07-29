63f700b47d30a546ca0a451e378762c7560a8627
Vicky Cao <miavickcao@gmail.com>
2025-07-29 09:51:51 -0700

    Initial Commit

---
+++ Packed/PackedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackedApp.swift
+//  Packed
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PackedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
